4e9f3c799204b0b9948d99afdc18b38958a92106
Antonio Martins <carlos12antoni@gmail.com>
2019-12-18 23:39:19 -0300

    (feat) trabalhando com rotas

---
+++ resources/views/profile.blade.php
@@ -0,0 +1,8 @@
+<h1>Profile</h1>
+
+<div>
+    <a href="{{route('app.user')}}">User</a>
+    <a href="{{route('app.profile')}}">Profile</a>
+    <a href="{{route('app')}}">App</a>
+    <a href="{{route('meusprodutos')}}">Produtos</a>
+</div>
