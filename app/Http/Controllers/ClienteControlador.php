6cc194daa4123f7dc566f53bf98e8fab0f8399fd
Antonio Martins <carlos12antoni@gmail.com>
2019-12-28 22:43:19 -0300

    (feat) criando métodos edit e update

--- app/Http/Controllers/ClienteControlador.php
+++ app/Http/Controllers/ClienteControlador.php
@@ -89,7 +89,9 @@ class ClienteControlador extends Controller
      */
     public function edit($id)
     {
-        //
+        $clientes = session('clientes');
+        $cliente = $clientes[$id - 1];
+        return view('clientes.edit', compact(['cliente']));
     }
 
     /**
@@ -101,7 +103,10 @@ class ClienteControlador extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $clientes = session('clientes');
+        $clientes[$id - 1]['nome'] = $request->nome;
+        session(['clientes' => $clientes]);
+        return redirect()->route('clientes.index');
     }
 
     /**
